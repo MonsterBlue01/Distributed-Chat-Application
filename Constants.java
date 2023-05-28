b0e30232fb43c25f1d7cbdfbb07a6cbff4ed2214
monstblu <wdsrs020808@gmail.com>
2023-05-27 20:11:41 -0700

    05/27/2023 8:45 PM

--- Constants.java
+++
@@ -1,8 +0,0 @@
-public class Constants {
-    // Name of the topic
-    public static final String TOPIC_NAME = "SimpleAggr";
-    // The interval between message publications by the root (in milliseconds)
-    public static final int PUBLISH_INTERVAL = 5000;
-    // Number of nodes in the system for different tests
-    public static final int[] NODE_COUNTS = {10, 30, 1000};
-}
