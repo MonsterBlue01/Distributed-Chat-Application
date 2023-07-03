e9a6edf62b59323a9d1d079c325d889f10e7465c
Blue Monster <73154603+MonsterBlue01@users.noreply.github.com>
2023-07-03 23:18:32 +0000

    07/03/2023 4:15 PM

--- Client.java
+++ Client.java
@@ -1,3 +1,49 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client {
-    
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 1234;
+    private BufferedReader in;
+    private PrintWriter out;
+    private Socket socket;
+
+    public void startClient() {
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            // Start a new thread for receiving messages
+            new Thread(new ReceivedMessagesHandler()).start();
+
+            // Sending messages happens in the main thread
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNextLine()) {
+                out.println(scanner.nextLine());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ReceivedMessagesHandler implements Runnable {
+        public void run() {
+            try {
+                String message;
+                while ((message = in.readLine()) != null) {
+                    System.out.println("Received message: " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.startClient();
+    }
 }
