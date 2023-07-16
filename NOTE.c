411863213ef9d0a71f2f22199b29b8d93d73409b
Blue Monster <73154603+MonsterBlue01@users.noreply.github.com>
2023-07-16 00:23:46 +0000

    07/15/2023 5:30 PM

--- NOTE.c
+++ NOTE.c
@@ -3,3 +3,5 @@
 // 3. When two users log in from different places, the one that is forced to log in first (under consideration)
 // 4. How to store the password?
 // 5. Consider switching to a SQL database
+// 6. Convert time to a more user-friendly format
+// 7. Avoid the message sent by the client (not necessary)
